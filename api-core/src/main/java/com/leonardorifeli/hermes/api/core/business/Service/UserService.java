a8db8eecf5648a72d085ab1323e7ac35521577b1
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-13 00:48:15 -0300

    Fixed userservice em

--- api-core/src/main/java/com/leonardorifeli/hermes/api/core/business/Service/UserService.java
+++ api-core/src/main/java/com/leonardorifeli/hermes/api/core/business/Service/UserService.java
@@ -1,7 +1,18 @@
 package com.leonardorifeli.hermes.api.core.business.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+import java.util.List;
+
 public class UserService {
 	
+	@PersistenceContext(unitName = "user", type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
 	
-	
+	private EntityManager getEm() {
+		return this.entityManager;
+	}
+
 }
