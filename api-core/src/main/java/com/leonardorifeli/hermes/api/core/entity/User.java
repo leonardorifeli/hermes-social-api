9268f217d840f857f9222be791fc826bedc06300
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-13 00:14:12 -0300

    Added the user entity

---
+++ api-core/src/main/java/com/leonardorifeli/hermes/api/core/entity/User.java
@@ -0,0 +1,62 @@
+package com.leonardorifeli.hermes.api.core.entity;
+ 
+import javax.persistence.*;
+  
+@Entity
+@Table(name="user")
+public class User {
+	
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    
+    private String email;
+    
+    @OneToMany(mappedBy="repository",cascade=CascadeType.PERSIST)
+    private List<Repository> repositories = new ArrayList<Repository>(); 
+    
+    public List<Repository> getRepositories() {
+        return this.repositories;
+    }
+    
+    public void setRepositories(List<Repository> repositories) {
+        this.repositories = repositories;
+    }
+     
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+ 
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public void setEmail(String email) {
+    	this.email = email;
+    }
+    
+    public String getEmail() {
+    	return this.email;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+ 
+}
