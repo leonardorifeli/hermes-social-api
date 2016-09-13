1d2400a0b4f276ea5f57b42a9331e10feb0125d2
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-12 21:10:17 -0300

    Added the REST easy service

---
+++ public/src/main/java/com/leonardorifeli/hermes/social/api/RESTEasyService.java
@@ -0,0 +1,43 @@
+package com.leonardorifeli.hermes.social.api;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.*;
+
+@ApplicationPath("/api/v1")
+public class RESTEasyService extends Application {
+
+    private Set<Object> singletons = new HashSet<>();
+
+    public RESTEasyService() {
+        List<ClassLoader> classLoadersList = new ArrayList<>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false), new ResourcesScanner(), new TypeAnnotationsScanner())
+                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("com.leonardorifeli.hermes.social.api"))));
+
+        Set<Class<? extends Object>> allClasses = reflections.getTypesAnnotatedWith(javax.ws.rs.Path.class);
+        for (Class clazz : allClasses) {
+            try {
+                singletons.add(clazz.newInstance());
+            } catch (InstantiationException e) {
+            } catch (IllegalAccessException e) {
+            }
+        }
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return this.singletons;
+    }
+}
