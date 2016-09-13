7155abb272b1bafa81c60e586d240bfffc1c1902
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-13 00:14:24 -0300

    Added the repository entity

---
+++ api-core/src/main/java/com/leonardorifeli/hermes/api/core/entity/Repository.java
@@ -0,0 +1,51 @@
+package com.leonardorifeli.hermes.api.core.entity;
+ 
+import javax.persistence.*;
+  
+@Entity
+@Table(name="repository")
+public class Repository {
+	
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    
+    @ManyToOne
+    private User user;
+    
+    public Repository(String name) {
+        this.name = name;
+    }
+ 
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public User getUser() {
+        return this.user;
+    }
+ 
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+ 
+}
