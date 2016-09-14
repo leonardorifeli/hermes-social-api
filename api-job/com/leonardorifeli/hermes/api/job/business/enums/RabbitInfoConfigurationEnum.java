ad9698e6cd2699b06fdff0629402205daeb9cedf
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-14 03:44:51 -0300

    Fixed enum configuration

---
+++ api-job/com/leonardorifeli/hermes/api/job/business/enums/RabbitInfoConfigurationEnum.java
@@ -0,0 +1,26 @@
+package com.leonardorifeli.hermes.api.job.business.enums;
+
+public abstract class RabbitIntoConfigurationEnum {
+
+	private final static String SERVER = "172.17.0.2";
+    private final static String USERNAME = "guest";
+    private final static String PASSWORD = "guest";
+    private final static Integer PORT = 5672;
+
+    public static String getServer() {
+    	return SERVER;
+    }
+
+    public static String getUsername() {
+    	return USERNAME;
+    }
+
+    public static String getPassword() {
+    	return PASSWORD;
+    }
+
+    public static Integer getPort() {
+    	return PORT;
+    }
+
+}
