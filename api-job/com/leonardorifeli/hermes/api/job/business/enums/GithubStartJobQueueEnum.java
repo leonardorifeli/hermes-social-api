0867f04e1811798c054e2d222e8786a24f8fd86e
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-14 23:43:20 -0300

    Created a enum to job queue name in import start

---
+++ api-job/com/leonardorifeli/hermes/api/job/business/enums/GithubStartJobQueueEnum.java
@@ -0,0 +1,11 @@
+package com.leonardorifeli.hermes.api.job.business.enums;
+
+public abstract class GithubStartJobQueueEnum {
+
+	private final static String QUEUE_NAME = "github_start";
+
+    public static String getQueueName() {
+    	return QUEUE_NAME;
+    }
+
+}
