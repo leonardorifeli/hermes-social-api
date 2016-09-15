9cf47d6510f1346b8baaa6db2e350fdec2479141
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-14 23:44:30 -0300

    Fixed resource of import

--- api-public/src/main/java/com/leonardorifeli/hermes/social/api/GithubImportProccessResource.java
+++ api-public/src/main/java/com/leonardorifeli/hermes/social/api/GithubImportProccessResource.java
@@ -4,16 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Path("/import/github")
+@Produces("application/json")
 public class GithubImportProccessResource
-{	
-    @POST
-    @Path("")
-    public Response builderProccess(@PathParam("user") String user)
+{
+	
+    @GET
+    @Path("{username}")
+    public Response builderProccess(@PathParam("username") String user)
     {
         String result = "Hello, " + user;
         return Response.status(200).entity(result).build();
